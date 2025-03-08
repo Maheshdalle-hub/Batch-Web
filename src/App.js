053df36ee13db5ca469588ce7c4e3eb403646337
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-03-08 11:03:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Subjects from "./pages/Subjects";
 import Lectures from "./pages/Lectures";
 import VideoPlayer from "./pages/VideoPlayer";
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/subjects" element={<Subjects />} />
-      <Route path="/lectures/:subject" element={<Lectures />} />
-      <Route path="/video/:chapter" element={<VideoPlayer />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/subjects" element={<Subjects />} />
+        <Route path="/lectures/:subject" element={<Lectures />} />
+        <Route path="/video/:subject/:chapterIndex" element={<VideoPlayer />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
