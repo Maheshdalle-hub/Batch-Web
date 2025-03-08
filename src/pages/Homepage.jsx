7e013feb4fec332171667dade4ae2f27bbde5267
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-03-08 15:04:21 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
   return (
     <div className="container">
       <div className="click-box" onClick={() => navigate("/subjects")}>
-        <img src="YOUR_IMAGE_URL" alt="Aarambh Batch 2025-26" className="homepage-image" />
+        <img src="IMG_20250227_100103_636.jpg" alt="Aarambh Batch 2025-26" className="homepage-image" />
         <h1>Aarambh Batch 2025-26</h1>
       </div>
     </div>
