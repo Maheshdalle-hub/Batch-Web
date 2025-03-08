8b4bc13c9c4ee44591e3659455aaae9a3d2331d0
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-03-08 17:29:55 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/global.css";
+import imageUrl from "./IMG_20250227_100103_636.jpg";  // Replace with your actual file name
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -8,7 +9,7 @@ const Homepage = () => {
   return (
     <div className="container">
       <div className="click-box" onClick={() => navigate("/subjects")}>
-        <img src="IMG_20250227_100103_636.jpg" alt="Aarambh Batch 2025-26" className="homepage-image" />
+        <img src={imageUrl} alt="Aarambh Batch 2025-26" className="homepage-image" />
         <h1>Aarambh Batch 2025-26</h1>
       </div>
     </div>
