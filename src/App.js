1b9a2b1ec5e840f3279251b09dd7889cea9122b3
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-03-11 14:53:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Subjects from "./pages/Subjects";
 import Lectures from "./pages/Lectures";
-import ChapterLectures from "./pages/ChapterLectures"; // ✅ Added new page
 import VideoPlayer from "./pages/VideoPlayer";
+import BookSelection from "./pages/BookSelection"; // ✅ New page for books
+import ChapterSelection from "./pages/ChapterSelection"; // ✅ New page for chapters
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/subjects" element={<Subjects />} />
-      <Route path="/lectures/:subject" element={<Lectures />} />
-      <Route path="/lectures/:subject/:chapter" element={<ChapterLectures />} /> {/* ✅ New Route */}
-      <Route path="/video/:subject/:chapter/:lectureIndex" element={<VideoPlayer />} />
-      <Route path="/video/live" element={<VideoPlayer />} /> {/* ✅ Live Class Route */}
+      <Route path="/books/:subject" element={<BookSelection />} /> {/* ✅ New */}
+      <Route path="/chapters/:book" element={<ChapterSelection />} /> {/* ✅ New */}
+      <Route path="/lectures/:book/:chapterIndex" element={<Lectures />} />
+      <Route path="/video/:book/:chapterIndex" element={<VideoPlayer />} />
     </Routes>
   );
 }
