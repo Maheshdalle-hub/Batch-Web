11de4c926a6f46a8502e76c7c1a6ef49769125fc
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-03-11 10:35:09 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/global.css";
 import imageUrl from "./IMG_20250227_100103_636.jpg";  // ✅ Replace with your actual file name
+import mlogo from "./assets/ntmlogo.jpg";
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -9,7 +10,7 @@ const Homepage = () => {
   return (
     <div className="container">
       {/* ✅ Big Logo */}
-      <img src="https://images.app.goo.gl/mm63uNUVpmDpUFU99" alt="Logo" className="big-logo" />
+      <img src={mlogo} alt="Logo" className="big-logo" />
 
       <div className="click-box" onClick={() => navigate("/subjects")}>
         <img src={imageUrl} alt="Aarambh Batch 2025-26" className="homepage-image" />
