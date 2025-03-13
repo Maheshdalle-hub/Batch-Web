2e4d7dad750e271064ced74a4044f6437684a0ec
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-03-13 19:57:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Homepage from "./pages/Homepage";
 import Subjects from "./pages/Subjects";
 import Lectures from "./pages/Lectures";
 import VideoPlayer from "./pages/VideoPlayer";
-import ChapterLectures from "./pages/ChapterLectures"; // ✅ Keep this
+import ChapterLectures from "./pages/ChapterLectures";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/subjects" element={<Subjects />} />
-      <Route path="/lectures/:subject" element={<Lectures />} />
-      <Route path="/chapter-lectures/:subject/:chapterIndex" element={<ChapterLectures />} /> 
-      <Route path="/video/:subject/:chapterIndex" element={<VideoPlayer />} />
-      <Route path="/video/live" element={<VideoPlayer />} />  {/* ✅ Added Live Class Route */}
+      <Route path="/login" element={<Login />} />
+      
+      {/* ✅ Protected Routes */}
+      <Route path="/subjects" element={<ProtectedRoute><Subjects /></ProtectedRoute>} />
+      <Route path="/lectures/:subject" element={<ProtectedRoute><Lectures /></ProtectedRoute>} />
+      <Route path="/chapter-lectures/:subject/:chapterIndex" element={<ProtectedRoute><ChapterLectures /></ProtectedRoute>} />
+      <Route path="/video/:subject/:chapterIndex" element={<ProtectedRoute><VideoPlayer /></ProtectedRoute>} />
+      <Route path="/video/live" element={<ProtectedRoute><VideoPlayer /></ProtectedRoute>} />  
     </Routes>
   );
 }
