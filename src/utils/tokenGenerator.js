48f237c8fa5e8a20b7302516531ceb20d1bdebfb
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-03-26 11:38:37 +0530

    Delete src/utils/tokenGenerator.js

--- src/utils/tokenGenerator.js
+++
@@ -1,5 +0,0 @@
-export const generateUserToken = () => {
-  const token = Math.random().toString(36).substr(2, 10); // ✅ Unique 10-char token
-  localStorage.setItem("userToken", token);
-  return token;
-};
